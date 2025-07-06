eb68e824dd30e40763b25623a68d813a4b7dfa11
Keyur Bhatiya <keyur@kabir.com>
2025-07-06 15:04:12 +0530

    railway1

--- academichub/settings.py
+++ academichub/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = 'django-insecure--j$ufrsdc^^$*_(7^#p4qj3pj3wawz52_j)mzut%f7xnj03rpy
 DEBUG = False
 
 # Add your allowed hosts for production
-ALLOWED_HOSTS = ['**']  # Replace with your actual domain or IP address
+ALLOWED_HOSTS = ['https://academichub.up.railway.app/']  # Replace with your actual domain or IP address
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
